e683d33b4c35ee55c2b77a5fb77218d1eb81194b
Wahaj Mansoor <wahajmansoor@hotmail.com>
2020-03-31 03:59:50 +0500

    Updates

---
+++ precache-manifest.01046784a7965a64b5e9c4a613673f86.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e4bf5624f618b252a21c206f439d7e1",
+    "url": "/ReactResort/index.html"
+  },
+  {
+    "revision": "1a888aef1a5adc8bbd05",
+    "url": "/ReactResort/static/css/main.57887953.chunk.css"
+  },
+  {
+    "revision": "1c114edf09e6b6274923",
+    "url": "/ReactResort/static/js/2.da9efca9.chunk.js"
+  },
+  {
+    "revision": "1a888aef1a5adc8bbd05",
+    "url": "/ReactResort/static/js/main.9bdce68d.chunk.js"
+  },
+  {
+    "revision": "93d34a3560a23cf85942",
+    "url": "/ReactResort/static/js/runtime~main.f861f487.js"
+  },
+  {
+    "revision": "5044e4b754822a0c326fdbe64daabe1a",
+    "url": "/ReactResort/static/media/defaultBcg.5044e4b7.jpeg"
+  },
+  {
+    "revision": "e9ea73949de71d45ae46047646b95727",
+    "url": "/ReactResort/static/media/loading-arrow.e9ea7394.gif"
+  },
+  {
+    "revision": "0927983efcfcadf41349bc4f4845bafe",
+    "url": "/ReactResort/static/media/noimage.0927983e.jpg"
+  },
+  {
+    "revision": "e928a5c462a9f4f590af64285b387571",
+    "url": "/ReactResort/static/media/room-1.e928a5c4.jpeg"
+  },
+  {
+    "revision": "052c76815f889f20b8a498c19a212c50",
+    "url": "/ReactResort/static/media/room-2.052c7681.jpeg"
+  }
+]);
